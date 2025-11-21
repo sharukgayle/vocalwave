227dce605aa31d00524592d2a6f84a2fa6212c26
Sharukh Ghayal <codingkavirus@gmail.com>
2025-11-21 13:06:52 +0530

    Add files via upload

---
+++ frontend/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{fontFamily:"sans-serif", textAlign:"center", marginTop:"20%"}}>
+      <h1>Speakify</h1>
+      <p>Your AI voice generator will appear here soon.</p>
+    </main>
+  );
+}
